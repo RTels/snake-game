50bfd67ffd501dc872d249819f4cfbf5cc2d8a1b
Rafael <rrft@cin.ufpe.br>
2023-09-21 19:01:22 -0300

    Added a .txt file to keep track of the highscore

--- scoreboard.py
+++ scoreboard.py
@@ -1,16 +1,18 @@
 from turtle import Turtle
 
-
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.hideturtle()
-        self.high_score = 0
+        with open("../../Desktop/data.txt") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.penup()
         self.color("white")
         self.goto(0, 260)
         self.update_scoreboard()
+
+
 
     def update_scoreboard(self):
         self.clear()
@@ -23,5 +25,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("../../Desktop/data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
         self.score = 0
         self.update_scoreboard()
